f8d00cc05e0d5f04d495f43c1c01772820d3279b
Ionică Bizău <bizauionica@gmail.com>
2023-02-15 22:08:41 +0100

    :arrow_up: 2.0.0 :tada:

--- example/index.js
+++ example/index.js
@@ -3,17 +3,21 @@
 const parseDbUri = require("../lib")
 
 console.log(parseDbUri("mysql://root:42@localhost:3306/database_name"))
-// { protocols: [ 'mysql' ],
+// {
+//   protocols: [ 'mysql' ],
 //   protocol: 'mysql',
 //   port: 3306,
 //   resource: 'localhost',
+//   host: 'localhost',
 //   user: 'root',
+//   password: '42',
 //   pathname: '/database_name',
 //   hash: '',
 //   search: '',
 //   href: 'mysql://root:42@localhost:3306/database_name',
 //   query: {},
+//   parse_failed: false,
 //   uri: 'mysql://root:42@localhost:3306/database_name',
 //   database: 'database_name',
-//   password: '42',
-//   dialect: 'mysql' }
+//   dialect: 'mysql'
+// }
