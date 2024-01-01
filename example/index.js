ed1f5f37f19be7f0c862ecdc0176834827e457b1
Ionică Bizău <bizauionica@gmail.com>
2024-01-01 12:49:10 +0200

    Support for sqlite

--- example/index.js
+++ example/index.js
@@ -21,3 +21,22 @@ console.log(parseDbUri("mysql://root:42@localhost:3306/database_name"))
 //   database: 'database_name',
 //   dialect: 'mysql'
 // }
+
+console.log(parseDbUri("sqlite://data/db.sqlite3"))
+// => {
+//   protocols: [ 'sqlite' ],
+//   protocol: 'sqlite',
+//   port: 0,
+//   resource: 'data',
+//   host: 'data',
+//   user: '',
+//   password: '',
+//   pathname: 'data/db.sqlite3',
+//   hash: '',
+//   search: '',
+//   href: 'sqlite://data/db.sqlite3',
+//   query: {},
+//   parse_failed: false,
+//   uri: 'sqlite://data/db.sqlite3',
+//   dialect: 'sqlite'
+// }
